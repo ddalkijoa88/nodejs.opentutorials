e0dbbc77da7c90607f9ce9f9c2eb4490283fa40d
min <ddalkijoa88@naver.com>
2020-03-31 22:25:10 +0900

    글 수정하고 수정된 내용 저장하기

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var url = require('url');
 var qs = require('querystring');
 
 // 홈페이지를 구성하는 HTML 기본 태그에 관한 함수
-function templateHTML(title, list, body) {
+function templateHTML(title, list, body, control) {
   return `
   <!doctype html>
   <html>
@@ -15,7 +15,7 @@ function templateHTML(title, list, body) {
   <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
-    <a href="/create">create</a>
+    ${control}
     ${body}
   </body>
   </html>`;
@@ -53,7 +53,9 @@ var app = http.createServer(function (request, response) {
         var description = 'Hello, Node.js';
 
         var list = templateList(filelist);
-        var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+        var template = templateHTML(title, list,
+            `<h2>${title}</h2>${description}`,
+            `<a href="/create">create</a>`);
 
         response.writeHead(200);
         response.end(template);
@@ -63,7 +65,9 @@ var app = http.createServer(function (request, response) {
         // path가 '/'이면서 쿼리 스트링이 있는 경우 호출할 내용
         fs.readFile(`data/${title}`, 'utf8', function (err, description) {
           var list = templateList(filelist);
-          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+          var template = templateHTML(title, list,
+              `<h2>${title}</h2>${description}`,
+              `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
 
           response.writeHead(200);
           response.end(template);
@@ -75,13 +79,14 @@ var app = http.createServer(function (request, response) {
       // console.log(filelist);
       var title = 'WEB - create';
       var list = templateList(filelist);
-      var template = templateHTML(title, list, `
-        <form action="http://localhost:3000/create_process" method="post">
+      var template = templateHTML(title, list,
+          `
+        <form action="/create_process" method="post">
             <p><input type="text" name="title" placeholder="title"></p>
             <p><textarea name="description" placeholder="description"></textarea></p>
             <p><input type="submit"></p>
         </form>
-      `);
+      `, '');
 
       response.writeHead(200);
       response.end(template);
@@ -99,9 +104,55 @@ var app = http.createServer(function (request, response) {
       // console.log(post);
       // console.log(post.title);
       // console.log(title);
+
+      // 입력받은 데이터를 파일에 저장
+      fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
+        response.writeHead(200);
+        // create_process페이지에서는 입력받은 데이터를 파일로 저장하는 것만 수행한다
+        // 따라서 특정 내용 수행 후 페이지를 이동시켜 줘야한다(page redirection = 302)
+        // 입력한 내용을 확인할 수 있는 페이지로 이동시킨다
+        response.writeHead(302, {location:`/?id=${title}`})
+        response.end();
+      })
     });
-  response.writeHead(200);
-  response.end('sucess');
+  } else if(pathname === '/update'){
+    fs.readdir('./data', function (error, filelist) {
+      // path가 '/'이면서 쿼리 스트링이 있는 경우 호출할 내용
+      fs.readFile(`data/${title}`, 'utf8', function (err, description) {
+        var list = templateList(filelist);
+        var template = templateHTML(title, list,
+            `
+            <form action="/update_process" method="post">
+              <p><input type="hidden" name="id" value="${title}"></p>
+              <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+              <p><textarea name="description" placeholder="description">${description}</textarea></p>
+              <p><input type="submit"></p>
+            </form>
+            `,
+            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
+
+        response.writeHead(200);
+        response.end(template);
+      });
+    });
+  } else if(pathname === '/update_process'){
+    var body = '';
+    request.on('data', function (data) {
+      body += data;
+    });
+    request.on('end', function () {
+      var post = qs.parse(body);
+      var id = post.id;
+      var title = post.title;
+      var description = post.description;
+      fs.rename(`data/${id}`, `data/${title}`, function (err) {
+        fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
+          response.writeHead(302, {location: `/?id=${title}`});
+          response.end();
+        })
+      })
+      // console.log(post);
+    });
   } else {
     // pathname이 '/'이 아닌 경우 에러 페이지 출력.
     //ex) http://localhost:3000/sdfj 이렇게 들어오면 pathname이 /sdfj가 됨. 즉, '/' != '/sdfj'
